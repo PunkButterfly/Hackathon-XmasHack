5d2deb36814203ef487ec2b4c26fefe7911e1604
Kealfeyne <kealfeyneworkshop@gmail.com>
2022-12-17 13:05:48 +0300

    Add entities

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from ml_model.inference import *
 from features import get_confidence_sentences_ids, get_entities
-from view_document import view_document
+from view_document import view_document, view_factors
 from process_files import convert_file_to_text
 from annotated_text import annotated_text
 from ml_model.download_model import download_model
@@ -18,7 +18,7 @@ st.set_page_config(layout="wide")
 
 model = download_model()
 
-st.title("Классификатор документов онлайн")
+st.title("Классификатор документов")
 
 inputing_text_column, inputing_file_column = st.columns(2, gap="large")
 
@@ -65,12 +65,12 @@ try:
     output = st.session_state.output
 
     with controlling_column:
-        st.subheader("Настройки анализа")
+        st.subheader("Ключевые факторы")
 
         confidence_filter = st.slider("Уровень доверия", min_value=0.1, max_value=0.9, step=0.1, value=0.8)
         st.session_state.confidence_filter = confidence_filter
 
-        st.write("")
+        st.write(" ")
         class_id = 0
         annotated_text(
             (
@@ -78,7 +78,12 @@ try:
                 "", colors[class_id])
         )
         view_first_class = st.button("Выделить", key=f"view_first")
+        if view_first_class:
+            ids = get_confidence_sentences_ids(*output[2], quantile_param=confidence_filter).get(0, np.array([])).tolist()
+            view_factors(st.session_state.document_content,
+                         ids[0:min(len(ids), 15)], colors[0])
 
+        st.write("___")
         st.write("")
         class_id = 1
         annotated_text(
@@ -87,7 +92,12 @@ try:
                 "", colors[class_id])
         )
         view_second_class = st.button("Выделить", key=f"view_second")
+        if view_second_class:
+            ids = get_confidence_sentences_ids(*output[2], quantile_param=confidence_filter).get(1, np.array([])).tolist()
+            view_factors(st.session_state.document_content,
+                         ids[0:min(len(ids), 15)], colors[1])
 
+        st.write("___")
         st.write("")
         class_id = 2
         annotated_text(
@@ -96,7 +106,12 @@ try:
                 "", colors[class_id])
         )
         view_third_class = st.button("Выделить", key=f"view_third")
+        if view_third_class:
+            ids = get_confidence_sentences_ids(*output[2], quantile_param=confidence_filter).get(2, np.array([])).tolist()
+            view_factors(st.session_state.document_content,
+                         ids[0:min(len(ids), 15)], colors[2])
 
+        st.write("___")
         st.write("")
         class_id = 3
         annotated_text(
@@ -105,7 +120,12 @@ try:
                 "", colors[class_id])
         )
         view_fourth_class = st.button("Выделить", key=f"view_fourth")
+        if view_fourth_class:
+            ids = get_confidence_sentences_ids(*output[2], quantile_param=confidence_filter).get(3, np.array([])).tolist()
+            view_factors(st.session_state.document_content,
+                         ids[0:min(len(ids), 15)], colors[3])
 
+        st.write("___")
         st.write("")
         class_id = 4
         annotated_text(
@@ -114,9 +134,19 @@ try:
                 "", colors[class_id])
         )
         view_fifth_class = st.button("Выделить", key=f"view_fifth")
+        if view_fifth_class:
+            ids = get_confidence_sentences_ids(*output[2], quantile_param=confidence_filter).get(4, np.array([])).tolist()
+            view_factors(st.session_state.document_content,
+                         ids[0:min(len(ids), 15)], colors[4])
 
+        st.write("___")
+        st.write("")
         # entities = get_entities(st.session_state.document_content)
-        # st.write(entities)
+        st.subheader("Важные сущности")
+        st.write("Локации (Открыть на карте?)")
+        loc = "Привет"
+        st.write(f"[loc](https://www.google.com/search?q={loc})")
+        st.write("Организации (Погуглить про нее?)")
 
     with viewing_column:
         confidence_filter = st.session_state.confidence_filter
--- view_document.py
+++ view_document.py
@@ -1,4 +1,6 @@
 import re
+import numpy as np
+import streamlit as st
 from annotated_text import annotated_text
 from ml_model.inference import splitting_text_by_regex, process_text, process_splitted_text
 
@@ -13,12 +15,38 @@ def process_input(text):
     return result
 
 
+def view_factors(document_text, ids, color="#630606"):
+    splitted_text, splitting_points = splitting_text_by_regex(process_text(document_text, lower_flag=False),
+                                                              splitter='([\t\n]\s*\d+[0-9\.]*\.\s)')
+    splitted_text = process_splitted_text(splitted_text)
+    glued_sentences = np.array(glue_sentences(splitted_text))[ids]
+    colored_factors = [(item, "", color) for item in glued_sentences]
+
+    for index in range(len(colored_factors)):
+        annotated_text(colored_factors[index])
+
+
 def view_document(document_text, ids, color="#630606"):
     splitted_text, splitting_points = splitting_text_by_regex(process_text(document_text, lower_flag=False),
                                                               splitter='([\t\n]\s*\d+[0-9\.]*\.\s)')
     splitted_text = process_splitted_text(splitted_text)
     # text_splitted = re.split(r"([\t\n]\s*\d+[0-9\.]*\.\s)", document_text)  # (\d+[0-9\.]*\.\s)
 
+    glued_sentences = glue_sentences(splitted_text)
+
+    colored_factors = []
+    for index in range(len(glued_sentences)):
+        if index in ids:
+            colored_factors.append(
+                (glued_sentences[index], "", color))  # "#1C6758" "#25316D" "#4C0033" "#4C3A51" "#630606"
+        else:
+            colored_factors.append(glued_sentences[index])
+
+    for index in range(len(colored_factors)):
+        annotated_text(colored_factors[index])
+
+
+def glue_sentences(splitted_text):
     with_points_splitted = [splitted_text[0]]
     index = 1
     while index < len(splitted_text) - 1:
@@ -30,13 +58,4 @@ def view_document(document_text, ids, color="#630606"):
             with_points_splitted.append(splitted_text[index])
             index += 1
 
-    colored_factors = []
-    for index in range(len(with_points_splitted)):
-        if index in ids:
-            colored_factors.append(
-                (with_points_splitted[index], "", color))  # "#1C6758" "#25316D" "#4C0033" "#4C3A51" "#630606"
-        else:
-            colored_factors.append(with_points_splitted[index])
-
-    for index in range(len(colored_factors)):
-        annotated_text(colored_factors[index])
+    return with_points_splitted
